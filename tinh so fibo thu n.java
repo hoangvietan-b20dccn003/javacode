f60ac46eb563da724ff4fa8646fb32c151456b7f
hoangvietan-b20dccn003 <147567154+hoangvietan-b20dccn003@users.noreply.github.com>
2025-01-10 06:09:25 +0700

    Update tinh so fibo thu n.java

--- tinh so fibo thu n.java
+++ tinh so fibo thu n.java
@@ -1,38 +1,58 @@
+/*Cho số nguyên dương n. Hãy kiểm tra xem n có phải là số trong dãy Fibonacci hay không?
+
+Input:
+
+Dòng đầu tiên đưa vào số lượng bộ test T.
+Những dòng kế tiếp đưa vào các bộ test. Mỗi bộ test là một số nguyên dương n.
+T, n thỏa mãn ràng buộc :1 ≤ T ≤ 100; 0≤n≤1018.
+Output:
+
+Đưa ra “YES” hoặc “NO” tương ứng với n là số Fibonacci hoặc không phải số Fibonacci của mỗi test theo từng dòng.*/
+
+package lamlai;
+
+import java.util.Scanner;
+
 public class test {
 
-//	public static boolean check(String s) {
-//		int sum = 0;
-//		for (int i = 0; i < s.length(); i++) {
-//			sum += s.charAt(i) - '0';
+//	public static Map<Integer, Integer> phanTich(int n) {
+//		Map<Integer, Integer> soSoNT = new LinkedHashMap<>();
+//		for (int i = 2; i < Math.sqrt(n); i++) {
+//			int dem = 0;
+//			while (n % i == 0) {
+//				n /= i;
+//				dem++;
+//			}
+//			if (dem > 0)
+//				soSoNT.put(i, dem);
 //		}
-//		for (int i = 0; i < s.length() - 1; i++) {
-//			int diff = Math.abs(s.charAt(i) - s.charAt(i + 1));
-//			if (diff != 2) {
-//				return false;
-//			}	
-//		}
-//		return sum % 10 == 0;
+//		if (n > 1)
+//			soSoNT.put(n, 1);
+//		return soSoNT;
 //	}
-	public static long fibo(int n) {
+
+	public static boolean checkFibo(long n) {
 		if (n == 1)
-			return 1;
-		if (n == 2)
-			return 1;
+			return true;
 		long f1 = 1, f2 = 1, fn = 0;
-		for (int i = 3; i <= n; i++) {
+		while (fn < n) {
 			fn = f1 + f2;
 			f1 = f2;
 			f2 = fn;
 		}
-		return fn;
+		return fn == n;
 	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
 		sc.nextLine();
 		while (t-- > 0) {
-			int n = sc.nextInt();
-			System.out.println(fibo(n));
+			long n = sc.nextLong();
+			if (checkFibo(n))
+				System.out.println("YES");
+			else
+				System.out.println("NO");
 		}
 	}
 }
